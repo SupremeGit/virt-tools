eae71d4bbf203691defb2702e7d1c0a100280689
jss <jss.nospam@gmail.com>
2018-01-08 07:59:26 +1030

    Minor fixes to soe-build.sh

--- virt-soe/soe-build.sh
+++ virt-soe/soe-build.sh
@@ -3,6 +3,12 @@
 # soe_build_vms.sh - script to sequence some ops using:
 #    soe_create_vms.sh to: maniupulate libvirt vms
 #    several ansible playbooks to connect & install soe on hosts 
+#
+# Can call this script like:
+#    ~/src/github/virt-tools/virt-soe/soe-build.sh --vms "centos7 fedora"
+#
+# The soe-vm-control script is alao called like:
+#    ~/src/github/virt-tools/virt-soe/soe-vm-control.sh --vms "centos7 fedora" shutdown
 
 #soe-vm-control.sh operates on a group of vms defined from a libvirt template:
 #  available operations: create, define, undefine, define, reimage, refresh, start, destroy, save. restore, shutdown, reboot, reset
@@ -15,23 +21,53 @@ vault="--vault-password-file ~/.ansible_vault_password"              #${vault}
 hostsfile="-i /etc/ansible/hosts"                                    #${hostsfile} 
 domain="soe.vorpal"     #vm "domain" to use when creating vms
 hostgroup=${domain%.*}  #use leftmost part of domain for ansible hostgroup
+SSHD_DELAY=10           #extra delay to wait for ssh.
 
 #vms to operate on:
 #def_vm_names="centos7 fedora ubuntu ubuntu_server temp foo bar"
 #def_vm_names="centos7 fedora ubuntu_server temp"
 #def_vm_names="centos7 temp"
 def_vm_names="centos7"
+vm_names=""
 
 function soe-set-vm_names () {
     export vm_names="${def_vm_names}"
     vm_fq_names=""
     for i in ${vm_names} ; do 
+	#echo "Adding ${i} to vm_fq_names=${vm_fq_names}"
+	###vm_fq_names+="${i}.${domain}."
 	vm_fq_names+="${i}.${domain} "
+	#echo "vm_fq_names=${vm_fq_names}"
     done
+    #vm_fq_names=${vm_fq_names%*.} #strip trailing ,
+    vm_fq_names=${vm_fq_names%* } #strip trailing " "
     export vm_fq_names
 }
-#when not set here, vm_names and vm_fq_names are taken from environment:
-soe-set-vm_names  
+function usage () {
+    echo "Usage:"
+    exit
+}
+
+function process_args () {
+    #call like: process_args "$@"
+    if [[ "$#" == "0" ]]; then
+	echo "No arguments. Halp!"
+	usage
+    fi
+    local ok=1
+    while (( "$#" )); do
+	case ${1} in          #switches for this shell script begin with '--'
+            -h | help)        usage;;
+            -d | --debug )    export debug=1; export DEBUG=echo ; echo -e "\nDebug mode on.";;
+            #--vms)           export vm_names="$2"   ; echo "VMs = ${vm_names}" ; shift ;;
+            --vms)            shift ; export def_vm_names="$@" ; soe-set-vm_names ; echo "VMs = ${vm_names}" ; shift ;;
+            *)                ok=0 ; echo "Unrecognised option: ${1}" ;  usage ;;
+	esac;
+	shift
+    done
+    if [ $ok -eq 0 ] ; then echo "Halp. Something isn't right with the command arguments. Exiting." ; usage ; fi
+    echo
+}
 
 function soe-vm-control () {
     #usage> soe-vm-control "operation" --vms "${vm_names}"
@@ -81,7 +117,6 @@ function vm-wait-boot () {
 	vm-test-boot
 	up=$?
     done
-    SSHD_DELAY=5
     echo "Waiting ${SSHD_DELAY} seconds extra for sshd to come up."
     sleep ${SSHD_DELAY} #qemu guest agent comes up fast, so, wait 5 secs for ssh
 }
@@ -173,6 +208,7 @@ function sequence-full () {
 }
 function sequence-partial () {
     echo "Running ad-hoc sequence of commands:"   #comment or uncomment as desired:
+    #soe-vm-control-vms "status"
 
     #vm-boot
     #or:
@@ -184,7 +220,7 @@ function sequence-partial () {
     #vm-wait-boot
 
     #vm-ansible-setup 
-    #vm-ansible-run-soe
+    vm-ansible-run-soe
 
     #vm-shutdown
     #or:
@@ -200,13 +236,17 @@ function sequence-test () {
 }
 #set-x-on
 ########################################Start invoking commands here:
+#when not set here, vm_names and vm_fq_names are taken from environment, or from $@:
+soe-set-vm_names  
+process_args $@    #mainly just process --vm-names "foo bar"
 msg_start
+
 #soe-vm-control-vms "status" --debug
 #soe-vm-control     "status" --vms "${vm_names}"
 #virsh list --all
 
-sequence-full
-#sequence-partial
+#sequence-full
+sequence-partial
 #sequence-test 
 
 msg_finished
